b333724d7af7d045def31fc8ebad806f5a140485
David Pedroza Segoviano <david.pedroza.segoviano@gmail.com>
2019-08-26 06:23:11 -0500

    Hidding the info

--- dbmongo.py
+++ dbmongo.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://admin:admin@cluster0-qvpho.mongodb.net/test?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://<user>:<password>@cluster0-qvpho.mongodb.net/test?retryWrites=true&w=majority"
 
 
 def db_connect(MONGO_URI, db_name, col_name):
